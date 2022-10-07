2ea1b54d6185a903209b6d2764bb2fd987b50861
Kateryna <koljuchka93@gmail.com>
2022-10-08 00:08:07 +0300

    add files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { User } from "./User";
+import user from '../user.json'
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  <div>
+    <User user={user[0]} />
+  </div>
   );
 };
