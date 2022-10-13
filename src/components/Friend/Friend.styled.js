b9d171e0c3f8ed938fed7f76838fdd0a4c70ae2f
Kateryna <koljuchka93@gmail.com>
2022-10-14 01:12:42 +0300

    isOnline

--- src/components/Friend/Friend.styled.js
+++ src/components/Friend/Friend.styled.js
@@ -19,7 +19,7 @@ export const Status = styled.span`
   width: 15px;
   height: 15px;
   border-radius: 50%;
-  //background-color: ${(p)=> {
+  background-color: ${(p)=> {
     return p.isOnline ? "green" : "red" 
     }
   };
